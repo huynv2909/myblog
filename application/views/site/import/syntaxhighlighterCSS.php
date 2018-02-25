89a7598384fe9d95a26f4c7fb813fd782c2fb258
HuyNV <huynv2909@gmail.com>
2018-02-26 00:34:08 +0700

    26/02

---
+++ application/views/site/import/syntaxhighlighterCSS.php
@@ -0,0 +1,3 @@
+	<!-- Syntax Highlighter  -->
+    <link rel="stylesheet" type="text/css" href="<?php echo public_url(); ?>css/syntax/shCore.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo public_url(); ?>css/syntax/shThemeDefault.css">
---
+++ application/views/site/import/syntaxhighlighterJS.php
@@ -0,0 +1,18 @@
+    <!-- Syntax Highlighter Javascript File  -->
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shCore.js"></script>
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushCss.js"></script>
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushJScript.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushPerl.js"></script> -->
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushPhp.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushPlain.js"></script> -->
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushPython.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushRuby.js"></script> -->
+    <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushSql.js"></script>
+   <!--  <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushVb.js"></script> -->
+    <!-- <script type="text/javascript" src="<?php echo public_url(); ?>js/syntax/shBrushXml.js"></script> -->
+    
+	<!-- Syntax Highlighter Call Function -->
+	<script type="text/javascript">
+		SyntaxHighlighter.config.clipboardSwf = '<?php echo public_url(); ?>js/syntax/clipboard.swf';
+		SyntaxHighlighter.all();
+	</script>
